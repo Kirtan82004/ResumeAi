c22832afa43a62b7da4a684ceefb95baa2ba7aac
Kirtan Kumar <kirtankumar9310136575@gmail.com>
2025-08-07 14:24:18 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Helmet } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Helmet>
       <App />
-    </Helmet>
-    
   </StrictMode>
 );
